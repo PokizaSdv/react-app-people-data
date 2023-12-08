5e5626fe03f211b73e5c8d7fc880cdb66616d432
PokizaSdv <pokizasdv@gmail.com>
2023-12-07 19:33:05 -0500

    Create People Data in tabular view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+import { peopleData } from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>People Data</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                        <th>Profession</th>
+                        <th>Yearly Salary</th>
+                        <th>Credit Score</th>
+                        <th>Yearly Saving</th>
+                        <th>Average Monthly Expense</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {peopleData.map((person, idx) => {
+                        <tr key={idx}>
+                            <td>{person.firstName}</td>
+                            <td>{person.lastName}</td>
+                            <td>{person.age}</td>
+                            <td>{person.profession}</td>
+                            <td>{person.finance.yearlySalary}</td>
+                            <td>{person.finance.yearlyTotalExpenses}</td>
+                            <td>{person.finance.creditScore}</td>
+                            <td>{person.finance.yearlySalary - person.finance.yearlyTotalExpenses}</td>
+                            <td>{person.finance.yearlyTotalExpenses / 12}</td>
+                        </tr>;
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default App;
+export { App };
