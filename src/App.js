f9478e579bd73921add512c2da741936a57adede
PokizaSdv <pokizasdv@gmail.com>
2023-12-07 20:00:51 -0500

    Find Max Salary and Min Salary Person

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import "./App.css";
 import { peopleData } from "./data";
 
 function App() {
+    let maxSalaryPerson = peopleData[0];
+
+    for (let i = 1; i < peopleData.length; i++) {
+        if (
+            peopleData[i].finance.yearlySalary >
+            maxSalaryPerson.finance.yearlySalary
+        ) {
+            maxSalaryPerson = peopleData[i];
+        }
+    }
+
+    let minSalaryPerson = peopleData[0];
+
+    // Find the person who made the least
+    for (let i = 1; i < peopleData.length; i++) {
+        if (
+            peopleData[i].finance.yearlySalary <
+            minSalaryPerson.finance.yearlySalary
+        ) {
+            minSalaryPerson = peopleData[i];
+        }
+    }
+
     return (
         <div className="App">
             <h1>People Data</h1>
@@ -22,22 +45,46 @@ function App() {
                 </thead>
                 <tbody>
                     {peopleData.map((person, idx) => {
-                        <tr key={idx}>
-                            <td>{person.firstName}</td>
-                            <td>{person.lastName}</td>
-                            <td>{person.age}</td>
-                            <td>{person.profession}</td>
-                            <td>{person.finance.yearlySalary}</td>
-                            <td>{person.finance.yearlyTotalExpenses}</td>
-                            <td>{person.finance.creditScore}</td>
-                            <td>{person.finance.yearlySalary - person.finance.yearlyTotalExpenses}</td>
-                            <td>{person.finance.yearlyTotalExpenses / 12}</td>
-                        </tr>;
+                        return (
+                            <tr key={idx}>
+                                <td>{person.firstName}</td>
+                                <td>{person.lastName}</td>
+                                <td>{person.age}</td>
+                                <td>{person.profession}</td>
+                                <td>{person.finance.yearlySalary}</td>
+                                <td>{person.finance.yearlyTotalExpenses}</td>
+                                <td>{person.finance.creditScore}</td>
+                                <td>
+                                    {person.finance.yearlySalary -
+                                        person.finance.yearlyTotalExpenses}
+                                </td>
+                                <td>
+                                    {person.finance.yearlyTotalExpenses / 12}
+                                </td>
+                            </tr>
+                        );
                     })}
                 </tbody>
             </table>
+
+            <div>
+                <h3>Person who made the most:</h3>
+                <p>
+                    Name: {maxSalaryPerson.firstName} {maxSalaryPerson.lastName}
+                </p>
+                <p>Age: {maxSalaryPerson.age}</p>
+                <p>Amount made: {maxSalaryPerson.finance.yearlySalary}</p>
+            </div>
+            <div>
+                <h3>Person who made the least:</h3>
+                <p>
+                    Name: {minSalaryPerson.firstName} {minSalaryPerson.lastName}
+                </p>
+                <p>Age: {minSalaryPerson.age}</p>
+                <p>Amount made: {minSalaryPerson.finance.yearlySalary}</p>
+            </div>
         </div>
     );
 }
 
-export { App };
+export default App;
